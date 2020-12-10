605d9b9410520293b1552382ffea7c197882cbb6
Me <denvoloshko@gmail.com>
2020-12-10 17:58:44 +0300

    and another one

--- first-pr/src/App.vue
+++ first-pr/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data(){
+    return {
+      todos: ['Зарядка', 'Завтрак']
+    }
+  },
+  components: {      
+    TodoList
+  },
+  methods: {                                     
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
