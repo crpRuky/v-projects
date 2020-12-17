1382928315901963947151549dea1fd02b6c9bfc
Me <denvoloshko@gmail.com>
2020-12-17 17:16:52 +0300

    new

--- weather/src/App.vue
+++ weather/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <informer></informer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import informer from './components/informer.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+export default{
+  components: { informer },
+  name: 'FirstComponent',
+  data(){
+    return {text: 'Я новый компонент!'}
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ weather/src/components/informer.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app">
+    <div class="informer-container">
+
+      <div class="temperature">
+        <img src="img/thermometer.png">
+        <div class="temp-real">1</div>
+        <div class="temp-feel">-3.8</div>
+      </div>
+
+      <div class="flex-break"></div>
+
+      <div class="humidity">
+        <img src="img/humidity.png"> 12 <small>%</small>
+      </div>
+
+      <div class="vertical-divider"></div>
+
+      <div class="wind">
+        <img src="img/wind.png"> 12 <small>м/с</small>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'informer',
+  props: {
+    
+  }
+}
+</script>
+
+<style>
+.informer-container {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 400px;
+  min-height: 255px;
+  padding: 30px;
+  background-image: url('../assets/forest.png');
+  background-size: cover;
+  background-position: center center;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.75);
+}
+</style>
