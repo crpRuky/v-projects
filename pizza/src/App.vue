e9df8fbb1ff691ece172e91b19c4c13c3ebf4981
Me <denvoloshko@gmail.com>
2020-12-24 17:53:31 +0300

    new

---
+++ pizza/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+   <h2>Меню:</h2>
+   <pizza v-bind:pizzaName='"Пицца от школы программистов"' v-bind:pizzaPrice='599'>
+     <p>На самом деле не 599, а 600, но никто ведь не заметит отсутствие одного рубля со сдачи, верно?</p>
+   </pizza>
+   <pizza v-bind:pizzaName='"Пицца от 6Б"' v-bind:pizzaPrice='12'>
+     <p>Потому и 12 рублей</p>
+   </pizza>
+   <pizza v-bind:pizzaName='"Мандарины"' v-bind:pizzaPrice='"некоторое количество"'>
+     <p>ура новый год</p>
+   </pizza>
+  </div>
+</template>
+
+<script>
+import Pizza from './components/Pizza.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      
+    }
+  },
+  components: {
+    pizza: Pizza
+  } 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ pizza/src/components/Pizza.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="main">
+        <h2>{{pizzaName}}</h2>
+        <p>Цена: <b>{{pizzaPrice}}</b> белорусских рублей</p>
+
+        <div class="info">
+            <slot>
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['pizzaName','pizzaPrice'],    
+}
+</script>
+
+<style scoped>
+h2 {
+  margin-top:0px;
+  margin-bottom:0px;
+}
+p {
+  margin-top:0px;
+  margin-bottom:10px;
+  font-size:24px;
+}
+.info p {
+  font-size:18px;
+}
+
+.main {
+  padding:15px 0px 10px 0px;
+  margin-top:20px;
+  background-color:rgba(185, 185, 255, 0.25);
+}
+</style>
