050ebb5abed08c73c5cd371605c8b3ee7046df0a
Me <denvoloshko@gmail.com>
2021-01-14 17:34:38 +0300

    new project

---
+++ chat/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <container></container>
+    <chat-message>Hello World</chat-message>
+  </div>
+</template>
+
+<script>
+import container from './components/Container.vue'
+import chatMessage from './components/ChatMessage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    container,
+    chatMessage
+  },
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ chat/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  width: 764px;
+  margin: auto;
+  box-shadow: 0 0 15px 0 #5f5f5f73;
+}
+</style>
