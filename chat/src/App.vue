f8f5056b4b58459517a4e76f55b4ce6b33090347
Me <denvoloshko@gmail.com>
2021-01-14 18:31:06 +0300

    getting messages

--- chat/src/App.vue
+++ chat/src/App.vue
@@ -1,35 +1,48 @@
 <template>
   <div id="app">
-    <container></container>
-    <chat-message>Hello World</chat-message>
+    <container>
+      <chat-window>
+        <chat-message v-for="message in messages" v-bind:key="message" :username="message.author" :datetime="message.datetime">{{message.text}}</chat-message>
+      </chat-window>
+    </container>
   </div>
 </template>
 
 <script>
-import container from './components/Container.vue'
-import chatMessage from './components/ChatMessage.vue'
+import Container from './components/Container.vue'
+import ChatMessage from './components/ChatMessage.vue'
+import ChatWindow from './components/ChatWindow.vue'
 
 export default {
   name: 'App',
   components: {
-    container,
-    chatMessage
+    Container,
+    ChatMessage,
+    ChatWindow
   },
   data(){
     return {
-      
+      messages:[]
     }
+  },
+  methods:{
+    getMessages(){
+      this.axios.get('http://37.77.104.246/api/chat/getmessages.php')
+      .then((response) => {
+        this.messages = response.data;
+        console.log(this.messages)
+      })
+    }
+  },
+  mounted(){
+    this.getMessages();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
 </style>
